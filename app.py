e6cfae241551be1f8b68cde56d796829cd5a3fb2
Sina Ebrahimi <ebrahimisina78@gmail.com>
2021-04-30 10:55:38 +0430

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from datetime import datetime
 
 import click
@@ -8,7 +10,10 @@ import click
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chaduxppgekfxg:09a540543492fffd344964c98180c3aa8d8a51ccda61a54f313a3ea8b494ba1c@ec2-54-211-176-156.compute-1.amazonaws.com:5432/dbidd3mltgi075'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'something_secret'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Url(db.Model):
@@ -21,22 +26,57 @@ class Url(db.Model):
 
     def __repr__(self):
         return f"Url(('id', {self.id}), ('title', {self.title}), ('url', {self.url}), ('created_at', {self.created_at}))"
+ 
 
-def init_db():
-    db.create_all()
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables"""
-    init_db()
-    click.echo("Initialized the database!")
-
-
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def root():
+    if request.method == 'POST':
+        title = request.form['title']
+        surl = request.form['url']
+        
+        alert = "danger"
+        msg = None
+
+        if not title:
+            msg = "title is required!"
+        elif not surl:
+            msg = "url is required!"
+        elif Url.query.filter_by(url=surl).first() is not None:
+            msg = "url already in database!"
+        else:
+            url = Url()
+            url.title = title
+            url.url = surl
+            try:
+                db.session.add(url)
+                db.session.commit()
+                msg = "Url added successfully!"
+                alert = "success"
+            except Exception as err:
+                db.session.rollback()
+                msg = f"database Error: {err}"
+
+        flash(msg, alert)
+        return redirect(url_for('root'))
+    
     urls = Url.query.all()
-    return render_template('index.html', urls)
+    return render_template('index.html', urls=urls)
+
+
+@app.route('/delete/<url_id>')
+def delete(url_id):
+    msg = None
+    alert = 'danger'
+    Url.query.filter_by(id=url_id).delete()
+    try:
+        db.session.commit()
+        msg = "Url deleted successfully!"
+        alert = "success"
+    except Exception as err:
+        db.session.rollback()
+        msg = f"database Error: {err}"
+    flash(msg, alert)
+    return redirect(url_for('root'))
 
 
 if __name__ == '__main__':
