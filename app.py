4575f4b8e1b6e3f85b067b7ad727348e615c64bf
Sina Ebrahimi <ebrahimisina78@gmail.com>
2021-04-30 07:34:57 +0430

    test get DATABASE_URL

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+import os
 
 
 app = Flask(__name__)
 
 
+DATABASE_URI = os.getenv("DATABASE_URL", "test")
+
 @app.route("/")
 def root():
-    return "Hello from root"
+    return f"Hello from root\ndb_url: {DATABASE_URI}"
 
 
 if __name__ == '__main__':
